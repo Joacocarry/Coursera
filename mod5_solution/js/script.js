f4dff5d4bb25b5f8f7e6a7913e5b7dd24c4d8d25
Joacocarry <66073699+Joacocarry@users.noreply.github.com>
2020-08-02 22:01:55 -0300

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -83,7 +83,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  $ajaxUtils.handleResponse, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  $ajaxUtils.handleResponse(function (x){
+  console.log(x);
+  }), // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
