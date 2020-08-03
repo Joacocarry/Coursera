5c63bf82d29164d532c290365e80eb6c09f5a30c
Joacocarry <66073699+Joacocarry@users.noreply.github.com>
2020-08-03 17:59:46 -0300

    Update script.js

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -118,9 +118,9 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems(chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems('chosenCategoryShortName');
 
-      $dc.insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage)
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
